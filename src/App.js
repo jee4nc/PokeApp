450eacc31217e8e5fd2365ed140ccbda4aa9faab
JeanLinux <jeanc.aldea1@gmail.com>
2020-07-06 16:24:05 -0400

    React router works! Routes for react router dom working now :D!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from "react";
+//importar react router dom
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 //Importacion de componentes
 import TitleMain from "./components/atoms/TitleMain";
 import PokeNavbar from "./components/atoms/PokeNavbar";
 
 function App() {
   return (
-    <div>
-      <PokeNavbar />
-      <TitleMain />
-    </div>
+    <>
+      <Router>
+        <PokeNavbar />
+        <Switch>
+          <Route extact path="/pokedex">
+            <h1>Hola soy un pokedex uwu</h1>
+          </Route>
+          <Route extact path="/types">
+            <h1>Hola soy pagina de tipos</h1>
+          </Route>
+          <Route extact path="/regions">
+            <h1>Hola soy pagina de regiones</h1>
+          </Route>
+          <Route extact path="/objects">
+            <h1>Hola soy pagina de objetos</h1>
+          </Route>
+          <Route extact path="/">
+            <TitleMain />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
--- src/components/atoms/PokeNavbar.js
+++ src/components/atoms/PokeNavbar.js
@@ -1,19 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./../../styles/PokeNavbar.scss";
 
 const PokeNavbar = () => {
   return (
     <div className="NAV">
-      <div className="NAV__title title_1">
-        <div className="Logo__container">
-          <img className="Logo" src="./utils/pokemon.svg" alt="poke_logo" />
+      <Link to="/">
+        <div className="NAV__title title_1">
+          <div className="Logo__container">
+            <img className="Logo" src="./utils/pokemon.svg" alt="poke_logo" />
+          </div>
+          <p>PokeAPP</p>
         </div>
-        <p>PokeAPP</p>
+      </Link>
+
+      <div className="NAV__title">
+        <Link to="/pokedex">Pokedex </Link>
       </div>
-      <div className="NAV__title">Title2</div>
-      <div className="NAV__title">Title3</div>
-      <div className="NAV__title">Title4</div>
-      <div className="NAV__title">Title5</div>
+
+      <div className="NAV__title">
+        <Link to="/types">Tipos de Pokemon</Link>
+      </div>
+
+      <div className="NAV__title">
+        <Link to="/regions">Regiones</Link>
+      </div>
+
+      <div className="NAV__title">
+        {" "}
+        <Link to="/objects">Objetos</Link>
+      </div>
     </div>
   );
 };
