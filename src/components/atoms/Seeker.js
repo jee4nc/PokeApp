630a29dceaa13a0fef07afa07d2cea38a58ac5ce
JeanLinux <jeanc.aldea1@gmail.com>
2020-07-07 18:35:41 -0400

    REACT HOOK FORM ADDED

--- src/components/atoms/Seeker.js
+++ src/components/atoms/Seeker.js
@@ -1,13 +1,27 @@
 import React from "react";
 import "./../../styles/Seeker.scss";
+import { useForm } from "react-hook-form";
+
 const Seeker = () => {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
     <div>
       <div className="Container_Title">
         <h1>Seeker of Pokemon :D!</h1>
       </div>
       <div className="Container_Input">
-        <input className="InputSeeker" placeholder="Search your pokemon" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+            className="InputSeeker"
+            name="seeker_pokemon"
+            placeholder="Search your pokemon"
+            ref={register({ required: true })}
+          />
+          {errors.seeker_pokemon && <span>This field is required</span>}
+          <input type="submit" />
+        </form>
       </div>
     </div>
   );
